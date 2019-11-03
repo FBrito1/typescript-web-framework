5984274a16c53287de4b2fa9f49946e2c13a2a0a
brito <luan.tecno@gmail.com>
2019-11-03 12:10:34 -0300

    object composition

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { User } from "./models/User";
 
-const user = new User({ name: "New record", age: 10 });
-user.on("change", () => {
-  console.log("use wae");
+const user = new User({ id: 1, name: "mewename", age: 30 });
+user.on("save", () => {
+  console.log(user);
 });
-user.trigger("change");
 
-console.log(user.get("name"));
+user.save();
