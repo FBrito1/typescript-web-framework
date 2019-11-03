c15876f2362000cf6a9e4dd3a392a57da95a3be6
brito <luan.tecno@gmail.com>
2019-11-03 08:24:31 -0300

    add User class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("HI");
+import { User } from "./models/User";
+
+const user = new User({ name: "Brito", age: 13 });
+
+user.on("change", () => {
+  console.log("1");
+});
+user.on("change", () => {
+  console.log("3");
+});
+user.on("save", () => {
+  console.log("save");
+});
+
+user.trigger("savettt");
---
+++ src/models/User.ts
@@ -0,0 +1,40 @@
+interface UserProps {
+  name?: string;
+  age?: number;
+}
+
+type Callback = () => void;
+
+export class User {
+  // example
+  // { 'click': [() => {}, () => {}] }
+  events: { [key: string]: Callback[] } = {};
+
+  constructor(private data: UserProps) {}
+
+  get(propName: string): string | number {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(callback => {
+      callback();
+    });
+  }
+}
